4db00eab5d663a7b4a8426452bf63d25da7ed4c3
trigiu <giu.tricomi@gmail.com>
2025-02-23 23:21:49 +0000

    altro step intermedio. problemi ancora con la parte di horizon-ui/iot che non riesce a ritrovare <<sembra>> le informazioni dal DB, che però sono presenti

---
+++ 2-new-mysql/create_dbs_UNIME_TEST.sql
@@ -0,0 +1,12 @@
+CREATE USER IF NOT EXISTS s4t_keystone@localhost IDENTIFIED BY 'Um3d3m0n';
+SET PASSWORD FOR s4t_keystone@localhost = PASSWORD('Um3d3m0n');
+
+CREATE USER IF NOT EXISTS s4t_iotronic@localhost IDENTIFIED BY 'Um3d3m0n';
+SET PASSWORD FOR s4t_iotronic@localhost = PASSWORD('Um3d3m0n');
+
+CREATE DATABASE s4t_keystone;
+GRANT ALL PRIVILEGES ON s4t_keystone.* TO 's4t_keystone'@'localhost' IDENTIFIED BY 'Um3d3m0n';
+GRANT ALL PRIVILEGES ON s4t_keystone.* TO 's4t_keystone'@'%' IDENTIFIED BY 'Um3d3m0n';
+CREATE DATABASE s4t_iotronic;
+GRANT ALL PRIVILEGES ON s4t_iotronic.* TO 's4t_iotronic'@'localhost' IDENTIFIED BY 'Um3d3m0n';
+GRANT ALL PRIVILEGES ON s4t_iotronic.* TO 's4t_iotronic'@'%' IDENTIFIED BY 'Um3d3m0n';
